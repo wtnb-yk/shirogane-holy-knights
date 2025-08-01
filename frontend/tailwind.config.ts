d43061ec372d35bd6b587eb7629981f3bfb03b85
wtnb-yk <theskyisthelimit.0819@gmail.com>
2025-08-02 02:35:51 +0900

    feat: Tailwind CSS設定を新規作成し、アニメーションプラグインを追加

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
+
+const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -65,5 +67,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
+
+export default config
