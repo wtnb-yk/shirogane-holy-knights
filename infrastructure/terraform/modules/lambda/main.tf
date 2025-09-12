7076c2d4cc676717db32ca44cc9c486b7ed4dd2c
wtnb-yk <theskyisthelimit.0819@gmail.com>
2025-09-12 21:24:49 +0900

    feat: update Lambda permission for API Gateway to remove alias dependency

--- infrastructure/terraform/modules/lambda/main.tf
+++ infrastructure/terraform/modules/lambda/main.tf
@@ -132,9 +132,8 @@ resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api.function_name
-  qualifier     = aws_lambda_alias.live.name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${var.api_gateway_execution_arn}/*/*"
   
-  depends_on = [aws_lambda_alias.live]
+  depends_on = [aws_lambda_function.api]
 }
