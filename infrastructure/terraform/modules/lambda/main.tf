4bf0a3eb47a0c3a1ce93f76d8c946d64ede3b4d3
wtnb-yk <theskyisthelimit.0819@gmail.com>
2025-09-12 21:10:09 +0900

    feat: update API Gateway integration to use Lambda invoke ARN and remove alias dependency

--- infrastructure/terraform/modules/lambda/main.tf
+++ infrastructure/terraform/modules/lambda/main.tf
@@ -132,11 +132,6 @@ resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api.function_name
-  qualifier     = aws_lambda_alias.live.name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${var.api_gateway_execution_arn}/*/*"
-  
-  depends_on = [
-    aws_lambda_alias.live
-  ]
 }
