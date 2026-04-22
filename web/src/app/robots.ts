17cccf17fca50c9e6ead16aa5d9800ad211cf726
wtnb-yk <theskyisthelimit.0819@gmail.com>
2026-04-23 01:20:31 +0900

    feat: update robots.txt to allow access to API OG endpoint

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/api/og'],
       disallow: '/api/',
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
